f0f7fa15223a627844b984e13ee93101041f8c94
Tiffany Azamar <tiffanyazamar@tiffanysmacbook.lan>
2020-09-10 14:06:08 -0600

    add new component and routers

--- chatelaine/src/app/app-routing.module.ts
+++ chatelaine/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:LoginComponent},
+  {path:'register', component:RegisterComponent},
+  {path:'userProfile', component:UserProfileComponent},
+  {path:'**', component:PageNotFoundComponent}
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- chatelaine/src/app/app.module.ts
+++ chatelaine/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserProfileComponent,
+    LoginComponent,
+    RegisterComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
